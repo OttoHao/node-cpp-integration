3e3308ab4492cb23d2587e9822f1ca32176b5990
Minghao PAN <MPan2@slb.com>
2021-08-24 20:56:56 +0800

    feat: add math cpp lib

---
+++ MathLibrary/MathLibrary/MathLibrary.cpp
@@ -0,0 +1,7 @@
+// MathLibrary.cpp : Defines the exported functions for the DLL.
+#include "pch.h" // use stdafx.h in Visual Studio 2017 and earlier
+#include "MathLibrary.h"
+
+int add(int i, int j) {
+    return i + j;
+}
